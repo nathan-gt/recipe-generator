68e6a9e73d6328d6763fe48d59391f39f731c406
Nathan <nathan.gt.protic@gmail.com>
2022-03-26 12:10:54 -0400

    broken link
    Signed-off-by: Nathan <nathan.gt.protic@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === "production" ? 'https://nathan-gt.github.io/recipe-generator/' : "/",
+    //publicPath: 'https://nathan-gt.github.io/recipe-generator/'
 };
